48f78455acc5cd988fd19cf782a2531642ee07ff
Cameron Katri <me@cameronkatri.com>
2022-02-12 23:50:52 -0500

    defaults 1.0.1

--- Sources/defaults.m
+++ Sources/defaults.m
@@ -23,7 +23,7 @@
  * SOFTWARE.
  */
 
-const unsigned char defaultsVersionString[] = "@(#)PROGRAM:defaults  PROJECT:defaults-1.0  BUILT:" __DATE__ " " __TIME__ "\n";
+const unsigned char defaultsVersionString[] = "@(#)PROGRAM:defaults  PROJECT:defaults-1.0.1\n";
 
 #import <CoreFoundation/CoreFoundation.h>
 #import <Foundation/Foundation.h>
